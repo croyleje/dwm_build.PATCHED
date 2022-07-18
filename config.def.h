5d2975193d8791813b5b562ceda16d91b301376c
croyleje <mail@jasoncroyle.com>
2022-07-18 19:56:47 -0400

    Updated config.def.h.

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 0;        /* vertical padding for statusbar */
+static const int vertpadbar         = 2;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#282a36";
@@ -40,7 +40,7 @@ static const Rule rules[] = {
 	 */
 	/* class        instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	/* { "BINARY",  NULL,     NULL,           1 << 8,    0,          -1,         0,         -1 }, */
-	{ "Alacritty",  NULL,     NULL,           0,         0,          -1,         0,         -1 },
+	{ "Alacritty",  NULL,     NULL,           0,         0,          1,          0,         -1 },
 	{ "St",         NULL,     NULL,           0,         0,          1,          0,         -1 },
 	{ NULL,         NULL,     "Event Tester", 0,         0,          0,          1,         -1 }, /* xev */
 };
