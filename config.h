99f46bdab309bd50394ffc047231b0d931976ad4
croyleje <mail@jasoncroyle.com>
2022-07-19 19:17:49 -0400

    PATCHED: dwm-focusmaster-return-20210804-138b405.diff.  IMPORTANT: key binding need to be manually added to config.def.h. BINDING: { MODKEY|ControlMask,           XK_space,  focusmaster,    {0} },

--- config.h
+++ config.h
@@ -126,6 +126,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                              6)
 	TAGKEYS(                        XK_8,                              7)
 	TAGKEYS(                        XK_9,                              8)
+    { MODKEY|ControlMask,           XK_space,  focusmaster,    {0} },
 	{ MODKEY|ShiftMask,             XK_q,              quit,           {0} },
 	{ MODKEY,                       XK_u,      focusurgent,    {0} },
 /* +	{ MODKEY|ShiftMask,             XK_BackSpace, quit,        {0} }, */
